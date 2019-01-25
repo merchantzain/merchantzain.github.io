9cb6fd577b67181154af30d9d834fff63efbf2c2
Zain Merchant <ztm140130@utdallas.edu>
2019-01-25 01:00:43 -0600

    Updated resume, added link to publication, and made it so you can click to move next in slideshow.

--- script.js
+++ script.js
@@ -31,11 +31,11 @@ function contact_me() {
 var slideIndex = 0;
 var slideImages = ["media/ucsd-e4e.jpg", "media/haystack.jpg", "media/utd-css.jpg"];
 var slideCaptions = [
-"This summer I was part of UC San Diego's Engineering for Exploration REU program. Our project was to build a working hexacopter with all control and command algorithms running on a Xilinx ZYNQ ARM + FPGA board.", 
+"This summer I was part of UC San Diego's Engineering for Exploration REU program. Our project was to build a working hexacopter with all control and command algorithms running on a Xilinx ZYNQ ARM + FPGA board. Learn more about what I did <a href=\"https://ieeexplore.ieee.org/document/8622102\" target=\"_blank\">here</a>.", 
 "For summer 2017, I was part of the MIT Haystack Observatory's REU program. That summer I worked on the avionics system for an air-dropped data collection device for antarctic research missions. Learn more about what I did <a href=\"https://www.haystack.mit.edu/edu/reu/2017/files/2017_Merchant_Poster.pdf\" target=\"_blank\">here</a>.", 
 "This is a quadrifilar helicoidal nested antenna system I made at the UT Dallas Center for Space Sciences to track satellites and record signals for measuring the total electron content (TEC) in the Ionosphere."];
 
-function slideshowFunction() {
+function slideshowFunction(isClicked) {
     document.getElementById("slideshow").src = slideImages[slideIndex];
     document.getElementById("slideshow_captions").innerHTML = slideCaptions[slideIndex];
     
@@ -45,5 +45,7 @@ function slideshowFunction() {
     	slideIndex += 1;
     }
     
-    setTimeout(slideshowFunction, 10000);
+    if (!isClicked) {
+        setTimeout(slideshowFunction, 10000);
+    }
 }
